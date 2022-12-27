5382eb4ac1582751bc71e6cc4819f8e523de337f
Yicheng Xia <yicheng200807@gmail.com>
2022-12-26 21:39:07 -0500

    Basic translation command to russian

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 import os
 import random
 import json
+from googletrans import Translator
+
 
 intents = discord.Intents.default()
 intents.members = True
@@ -10,6 +12,12 @@ intents.presences = True
 
 client = discord.Client(intents=intents)
 
+
+
+# TRANSLATOR
+def translate(text, dest='ru'):
+    translator = Translator()
+    return translator.translate(text, dest=dest).text
 
 @client.event
 async def on_ready():
@@ -24,15 +32,6 @@ async def send_dm(id, msg):
     await user.send(msg)
 
 
-# async def bg():
-#     await client.wait_until_ready()
-#     msg = ''
-#     channel = client.get_channel(901277480898412555)  # replace with channel_id
-#     while not client.is_closed():
-#         msg = input()
-#         await channel.send(msg)
-#         await asyncio.sleep(1.5)  # task runs every 60 seconds
-
 
 @client.event
 async def on_message(message):
@@ -44,6 +43,8 @@ async def on_message(message):
         await message.channel.send('PoNg!')
     elif message.content.startswith('$bing'):
         await message.channel.send('chilling')
+    elif message.content.startswith('$tr'):
+        await message.channel.send(translate(message.content[3:]))
 
     if "allez les bleu" in message.content.lower() or "la coupe" in message.content.lower():
         dms = ["I wish you step on lego in the middle of the night", "I hope your pillow is hot on both sides",
@@ -53,6 +54,9 @@ async def on_message(message):
         print(f"SYS | Dm sent: {rand_dm}")
 
 
-f = open('secret.json')
-TOKEN = json.load(f)['TOKEN']
-client.run(TOKEN)
+
+
+if __name__ == '__main__':
+    f = open('secret.json')
+    TOKEN = json.load(f)['TOKEN']
+    client.run(TOKEN)

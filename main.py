6e055f59ec9a62fa3e0c550a018201819c4cc606
Yicheng Xia <yicheng200807@gmail.com>
2022-12-26 22:06:54 -0500

    Translation cmd and auto lang detection over 80% confidence

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import random
 import json
 from googletrans import Translator
 
-
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
@@ -13,11 +12,21 @@ intents.presences = True
 client = discord.Client(intents=intents)
 
 
-
 # TRANSLATOR
 def translate(text, dest='ru'):
     translator = Translator()
     return translator.translate(text, dest=dest).text
+
+
+def detectLang(text):
+    translator = Translator()
+    detection = translator.detect(text)
+    lang = 'en'
+    if(detection.confidence >= 0.8):
+        lang = detection.lang
+
+    return lang
+
 
 @client.event
 async def on_ready():
@@ -26,11 +35,9 @@ async def on_ready():
     print('SYS | I\'m in')
 
 
-
 async def send_dm(id, msg):
     user = await client.fetch_user(id)
     await user.send(msg)
-
 
 
 @client.event
@@ -44,7 +51,20 @@ async def on_message(message):
     elif message.content.startswith('$bing'):
         await message.channel.send('chilling')
     elif message.content.startswith('$tr'):
-        await message.channel.send(translate(message.content[3:]))
+        if message.content.startswith('$tr_'):
+            try:
+                destLang = message.content[4:6]
+                if destLang == 'zh':
+                    destLang = 'zh-CN'  # Default to Simplified chinese
+                await message.channel.send(translate(message.content[6:], dest=destLang))
+            except ValueError:
+                await message.channel.send("Invalid translation request")
+        else:
+            await message.channel.send(translate(message.content[3:]))
+    elif detectLang(message.content) != 'en':
+        # elif detectLang(message.content) in ['ru', 'zh-CN', 'zh-TW']:
+        translation = translate(message.content, dest='en')
+        await message.channel.send(f'Translation: {translation}')
 
     if "allez les bleu" in message.content.lower() or "la coupe" in message.content.lower():
         dms = ["I wish you step on lego in the middle of the night", "I hope your pillow is hot on both sides",
@@ -52,8 +72,6 @@ async def on_message(message):
         rand_dm = random.choice(dms)
         await send_dm(message.author.id, rand_dm)
         print(f"SYS | Dm sent: {rand_dm}")
-
-
 
 
 if __name__ == '__main__':
